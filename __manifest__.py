b6cbdc0db552cbfaeef781cb39539d9e33351cb5
Alfonso Gonzalez <info@wsemantic.com>
2025-01-31 09:15:48 +0100

    vistas

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/shopify_instance.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
