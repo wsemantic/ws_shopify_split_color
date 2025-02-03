580b66bb8afa7254a498f327bb52c9cc2a21bda9
Alfonso Gonzalez <info@wsemantic.com>
2025-02-03 19:49:14 +0100

    Update sale_order.py

--- models/sale_order.py
+++ models/sale_order.py
@@ -9,13 +9,15 @@ class SaleOrder(models.Model):
         Si el JSON no incluye ni 'first_name' ni 'last_name', se asigna un valor por defecto
         (por ejemplo, usando el email o una cadena fija) antes de llamar al método original.
         """
+        _logger.info(f"WSSH Check customer {customer.get('id')}")
         # Si no hay ni first_name ni last_name, definimos un nombre por defecto
-        if not (customer.get('first_name') or customer.get('last_name')):
+        if not (customer.get('first_name') or customer.get('last_name')):            
             # Podemos usar el email o un texto fijo como nombre
             default_name = customer.get('email') or _("Shopify Customer")
             # Creamos una copia modificable del diccionario
             customer = dict(customer)
             customer['first_name'] = default_name
+            _logger.info(f"WSSH asignamos name {default_name}")
 
         # Llamamos al método original (heredado) que se encargará del resto
         return super(SaleOrder, self).check_customer(customer)
